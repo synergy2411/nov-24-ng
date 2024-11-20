4eae834080b512e47f44bd66285dc4fbb96c74e5
Sumit Khandelwal <synergy2411@outlook.com>
2024-11-20 15:46:50 +0530

    reactive form added

--- frontend-app/src/app/components/demo/auth/registration/registration.component.ts
+++ frontend-app/src/app/components/demo/auth/registration/registration.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
+  username = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
 
-  constructor() { }
+  registrationForm!: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      uname: this.username,
+      pwd: this.password,
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.registrationForm);
+  }
 }
